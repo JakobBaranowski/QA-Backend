4b3ba3ceeb371a95aef9e296f86ebb4a40d63e2a
Jakobyte <jakob@baranowski.cc>
2021-01-04 12:24:24 +0100

    Upgrade 3.4.1 & Cloud storage

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,30 @@
 module.exports = ({ env }) => ({
-    email: {
-      provider: 'smtp',
-      providerOptions: {
-        host: 'smtp.gmail.com', 
-        port: 465   , //SMTP Port
-        secure: true,
-        username: 'jakob@baranowski.cc',
-        password: env('GMAIL_PASS'),
-        rejectUnauthorized: true,
-        requireTLS: true,
-        connectionTimeout: 1,
-      },
-      settings: {
-        from: 'dev@baranowski.cc',
-        replyTo: 'dev@baranowski.cc',
-      },
+  email: {
+    provider: 'smtp',
+    providerOptions: {
+      host: 'smtp.gmail.com',
+      port: 465, //SMTP Port
+      secure: true,
+      username: 'jakob@baranowski.cc',
+      password: env('GMAIL_PASS'),
+      rejectUnauthorized: true,
+      requireTLS: true,
+      connectionTimeout: 1,
     },
-  });
+    settings: {
+      from: 'dev@baranowski.cc',
+      replyTo: 'dev@baranowski.cc',
+    },
+  },
+
+  upload: {
+    provider: 'google-cloud-storage',
+    providerOptions: {
+      bucketName: 'eu.artifacts.quaralender-298309.appspot.com',
+      publicFiles: false,
+      uniform: false,
+      basePath: '',
+    },
+  },
+
+});
